c19791a050cdd240dd60ac805033bac89274375a
alyssawulans <alyssawulansari@gmail.com>
2026-07-05 20:07:05 +0700

    Tugas_16

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,21 @@
 import java.util.Properties
+
+// 1. Ini untuk membaca Google Maps API Key & SDK (Mengatasi error Unresolved reference sebelumnya)
 val localProperties = Properties().apply {
     val f = rootProject.file("local.properties")
     if (f.exists()) {
         load(f.inputStream())
     }
 }
+
+// 2. Ini untuk membaca data Keystore / Akun Play Store kamu (Sesuai tutorial Play Store)
+val keystoreProperties = Properties().apply {
+    val f = rootProject.file("key.properties")
+    if (f.exists()) {
+        load(f.inputStream())
+    }
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -13,7 +24,7 @@ plugins {
 }
 
 android {
-    namespace = "com.app.presgo"
+    namespace = "com.app.presgoo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,13 +33,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    // Menggunakan kembali kotlinOptions bawaan Flutter yang sesuai versi Kotlin kamu
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.app.presgo"
+        applicationId = "com.app.presgoo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -39,11 +51,20 @@ android {
             localProperties.getProperty("GOOGLE_MAPS_API_KEY", "")
     }
 
+    signingConfigs {
+        if (keystoreProperties.isNotEmpty()) {
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/com/example/presgo/presgo_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/presgo/presgo_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app.presgo
+package com.app.presgoo
 
 import io.flutter.embedding.android.FlutterActivity
 
